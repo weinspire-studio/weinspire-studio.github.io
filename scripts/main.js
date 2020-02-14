6ee92a103796c834e2c79ec4c71385ed48c5b6fa
matias <matias.gonzalo.lopez@gmail.com>
2020-02-14 02:55:31 -0500

    fixed menu 1

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,20 @@ const navElements = document.querySelectorAll(".nav-list li");
 const navImg = document.querySelector("nav img");
 const siteWrapper = document.querySelector("#site-wrapper");
 const largeScreenMQ = window.matchMedia("(min-width: 1024px)");
+const navBar = document.querySelector("#navbar");
+const footer = document.querySelector("#footer");
+const svgArrow = document.querySelector("#footer #up-arrow");
 
 // mobile burger and menu
 // TODO: add the listener only on small resolutions
 burger.addEventListener("click", () => {
-  navList.classList.toggle("open");
-  burger.classList.toggle("cross");
   siteWrapper.classList.toggle("menu-open");
-  svgBackground.classList.toggle("svg-opacity");
-  heroText.classList.toggle("hero-text-opacity");
+  burger.classList.toggle("cross");
+  navList.classList.toggle("open");
   navImg.classList.toggle("logo-index");
+  heroText.classList.toggle("hero-text-opacity");
+  svgBackground.classList.toggle("svg-opacity");
+  footer.classList.toggle("footer-index");
   navElements.forEach((navEl, index) => {
     navEl.style.animationDelay = `${0.05 + index / 12.5}s`;
     navEl.classList.toggle("nav-link-anim");
@@ -70,25 +74,32 @@ function smallScreenCode() {
 }
 
 function styleNav() {
+  // let scro = siteWrapper.scrollHeight;
+  // console.log("height" + scro);
+  // console.log(window.innerHeight);
   let scrolledY = siteWrapper.scrollTop;
-  console.log(scrolledY);
-  let scro = siteWrapper.scrollHeight;
-  console.log("height" + scro);
-  // console.log(`changing nav ${i}`);
-  // i++;
+  // console.log(scrolledY);
+  if (scrolledY > 0) {
+    navBar.classList.add("nav-white");
+  } else {
+    navBar.classList.remove("nav-white");
+  }
 }
 
-const anchorHome = document.querySelector(".nav-home");
-const anchorContact = document.querySelector(".nav-contact");
+//// /////////// /////
+// TODO:
+// changes the href of a navLink depending on whether the site is in home or in another page.
+// const anchorHome = document.querySelector(".nav-home");
+// const anchorContact = document.querySelector(".nav-contact");
 
-console.log(anchorHome);
-console.log(window.location.href);
-console.log(anchorHome.href);
+// console.log(anchorHome);
+// console.log(window.location.href);
+// console.log(anchorHome.href);
 
-if (
-  window.location.href === "https://weinspire-studio.github.io/home" ||
-  window.location.href === "https://weinspire-studio.github.io/home/"
-) {
-  anchorHome.href = "/home/#home";
-  console.log("home");
-}
+// if (
+//   window.location.href === "https://weinspire-studio.github.io/home" ||
+//   window.location.href === "https://weinspire-studio.github.io/home/"
+// ) {
+//   anchorHome.href = "/home/#home";
+//   console.log("home");
+// }
