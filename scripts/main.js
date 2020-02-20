dd0b2f979b3b78673f5f297e736093bc3e8d6913
matias <matias.gonzalo.lopez@gmail.com>
2020-02-20 18:04:29 -0500

    anchor underline black & white

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
-// variables definition
+//VARIABLES
 const siteWrapper = document.querySelector("#site-wrapper");
 const burger = document.querySelector(".burger");
 const lineElements = document.querySelectorAll(".burger div");
 const nav = document.querySelector("nav");
+const navImg = document.querySelector("nav img");
 const navBar = document.querySelector("#navbar");
 const navList = document.querySelector(".nav-list");
 const navElements = document.querySelectorAll(".nav-list li");
-const navImg = document.querySelector("nav img");
+const navAnchors = document.querySelectorAll(".nav-list a");
 const navContainer = document.querySelector(".navigation-container");
 const navWhite = document.querySelector(".navigation-color-white");
 const navBlack = document.querySelector(".navigation-overlay-black");
@@ -16,12 +17,12 @@ const svgArrow = document.querySelector("#footer #up-arrow");
 const notMobileScreenMQ = window.matchMedia("(min-width: 600px)");
 // const svgBackground = document.querySelector("#svg-background");
 // const notMobileScreenMQ = window.matchMedia("(min-width: 600px)");
-const navAnchors = document.querySelectorAll(".nav-list a");
 
 let hasScrollListener = false;
 let hasClickListener = false;
+let hasHoverListener = false;
 
-// jQuery for animated scroll
+// jQuery for animated scroll.
 $("#up-arrow").on("click", function() {
   const siteWrapperTop = $("#site-wrapper").position().top;
   $("#site-wrapper").animate(
@@ -39,7 +40,7 @@ if (notMobileScreenMQ.matches) {
   smallScreenCode();
 }
 
-//adds listener that executes when screen width changes (passing by 1024px)
+//adds listener that executes when screen width changes (passing by 600px).
 notMobileScreenMQ.addListener(() => {
   if (notMobileScreenMQ.matches) {
     largeScreenCode();
@@ -48,17 +49,19 @@ notMobileScreenMQ.addListener(() => {
   }
 });
 
+//FUNCTIONS
+//code that executes only in desktop and tablet screens (> 600px).
 function largeScreenCode() {
   if (!hasScrollListener) {
     siteWrapper.addEventListener("scroll", styleNavOnScroll);
     hasScrollListener = true;
   }
-  // console.log("large screen code");
   restoreDesktopNav();
+  styleAnchorOnHover();
 }
 
+//code that executes only in cellphone screens (< 600px).
 function smallScreenCode() {
-  // console.log("small screen code");
   styleMobileNav();
   if (!hasScrollListener) {
     siteWrapper.addEventListener("scroll", styleNavOnScroll);
@@ -66,6 +69,32 @@ function smallScreenCode() {
   }
 }
 
+// animation effect (underline) for desktop nav anchors.
+function styleAnchorOnHover() {
+  if (!hasHoverListener) {
+    navAnchors.forEach(anchor => {
+      anchor.addEventListener("mouseover", function() {
+        let scrolledY = siteWrapper.scrollTop;
+        if (scrolledY === 0) {
+          anchor.classList.remove("anchor-white");
+          anchor.classList.remove("underline-black");
+          anchor.classList.add("underline-white");
+        } else {
+          anchor.classList.add("anchor-white");
+          anchor.classList.add("underline-black");
+          anchor.classList.remove("underline-white");
+        }
+        let property = new Object();
+        property.selector = "width";
+        property.value = `${(anchor.offsetWidth + 5) / 2}` + `px`;
+        editStyle(".nav-list a", property);
+      });
+    });
+  }
+  hasHoverListener = true;
+}
+
+//adds or removes classes in order to give white styles to the nav.
 function styleNavOnScroll() {
   let scrolledY = siteWrapper.scrollTop;
   scrolledY > 0
@@ -74,6 +103,7 @@ function styleNavOnScroll() {
       nav.classList.remove("nav-no-border"));
 }
 
+//appends navList to navContainer (because of burger z-index issue) and adds click listener to menu burger.
 function styleMobileNav() {
   navList.parentNode.removeChild(navList);
   navContainer.appendChild(navList);
@@ -91,6 +121,7 @@ function styleMobileNav() {
   hasClickListener = true;
 }
 
+// called when resizing from mobile to large screens, and when loading page on large resolutions, adds or removes nav classes for styling (calling) and append navList to child if resizing.
 function restoreDesktopNav() {
   // console.log(navContainer);
   if (navContainer.firstChild !== null) {
@@ -102,6 +133,7 @@ function restoreDesktopNav() {
   }
 }
 
+// adds or removes classes to nav and burger, and changes z-index and opacity to elements at the back (for black div when opening menu). Small and Large screens.
 function toggleNavClasses() {
   let scrolledYMobile;
   scrolledYMobile = siteWrapper.scrollTop;
@@ -120,34 +152,10 @@ function toggleNavClasses() {
   footer.classList.toggle("footer-index");
 }
 
-console.log(navAnchors[3].offsetWidth);
-console.log(navAnchors[0]);
-let asd = document.styleSheets[0].rules;
-// console.log(asd[10].selectorText);
-
-let property = new Object();
-property.selector = "width";
-// property.value = "20px";
-
-navAnchors.forEach(anchor => {
-  // anchor.style.width = anchor.offsetWidth
-  anchor.addEventListener("mouseover", function() {
-    // let property = new Object();
-    // property.selector = "width";
-    property.value = `${(anchor.offsetWidth + 5) / 2}` + `px`;
-    editStyle(".nav-list a", property);
-    // console.log("mouse over");
-  });
-  anchor.addEventListener("mouseleave", function() {
-    // this.classList.toggle("special");
-    // console.log("mouse leave");
-  });
-});
-
-//adds a custom property to an existing pseudo-class.
+//adds a custom property to an existing pseudo-class, used to define width of underlines, according to nav anchors lenght.
 function editStyle(className, property) {
   let sheet = document.styleSheets[0];
-  let sheetLength = sheet.cssRules.length;
+  let sheetLength = sheet.cssRules.length || sheet.rules.length;
   let classes = sheet.rules || sheet.cssRules;
   for (let x = 0; x < classes.length; x++) {
     if (classes[x].selectorText == className) {
@@ -163,22 +171,7 @@ function editStyle(className, property) {
   }
 }
 
-// function getStyle(className) {
-//   let cssText = "";
-//   let classes =
-//     document.styleSheets[0].rules || document.styleSheets[0].cssRules;
-//   for (let x = 0; x < classes.length; x++) {
-//     if (classes[x].selectorText == className) {
-//       cssText += classes[x].cssText || classes[x].style.cssText;
-//     }
-//   }
-//   return cssText;
-// }
-// let special = getStyle(".special");
-// alert(special);
-
 //// /////////// /////
-// TODO:
 // changes the href of a navLink depending on whether the site is in home or in another page.
 // const anchorHome = document.querySelector(".nav-home");
 // const anchorContact = document.querySelector(".nav-contact");
