3cb9d7b8b0738d7d16c3fd2a5135e9cf2eee9d19
matias <matias.gonzalo.lopez@gmail.com>
2020-02-27 22:31:24 -0500

    svg sprites, contact form, media queries

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ const navBlack = document.querySelector(".navigation-overlay-black");
 const heroText = document.querySelector(".hero-text");
 const footer = document.querySelector("#footer");
 const svgArrow = document.querySelector("#footer #up-arrow");
-const notMobileScreenMQ = window.matchMedia("(min-width: 600px)");
+const notMobileScreenMQ = window.matchMedia("(min-width: 801px)");
 // const svgBackground = document.querySelector("#svg-background");
 // const notMobileScreenMQ = window.matchMedia("(min-width: 600px)");
 
@@ -40,7 +40,7 @@ if (notMobileScreenMQ.matches) {
   smallScreenCode();
 }
 
-//adds listener that executes when screen width changes (passing by 600px).
+//adds listener that executes when screen width changes (passing by 801px).
 notMobileScreenMQ.addListener(() => {
   if (notMobileScreenMQ.matches) {
     largeScreenCode();
@@ -50,7 +50,7 @@ notMobileScreenMQ.addListener(() => {
 });
 
 //FUNCTIONS
-//code that executes only in desktop and tablet screens (> 600px).
+//code that executes only in desktop and tablet screens (> 801px).
 function largeScreenCode() {
   if (!hasScrollListener) {
     siteWrapper.addEventListener("scroll", styleNavOnScroll);
@@ -60,7 +60,7 @@ function largeScreenCode() {
   styleAnchorOnHover();
 }
 
-//code that executes only in cellphone screens (< 600px).
+//code that executes only in cellphone screens (< 801px).
 function smallScreenCode() {
   styleMobileNav();
   if (!hasScrollListener) {
@@ -130,6 +130,11 @@ function restoreDesktopNav() {
   }
   if (siteWrapper.classList.contains("menu-open")) {
     toggleNavClasses();
+    navElements.forEach(navEl => {
+      navEl.style.animationDelay = "";
+      navEl.classList.remove("nav-link-anim");
+      navEl.classList.remove("invisible");
+    });
   }
 }
 
@@ -152,7 +157,7 @@ function toggleNavClasses() {
   footer.classList.toggle("footer-index");
 }
 
-//adds a custom property to an existing pseudo-class, used to define width of underlines, according to nav anchors lenght.
+//adds a custom property to an existing pseudo-class. Used to define width of underlines, according to nav anchors length.
 function editStyle(className, property) {
   let sheet = document.styleSheets[0];
   let sheetLength = sheet.cssRules.length || sheet.rules.length;
@@ -169,6 +174,174 @@ function editStyle(className, property) {
       );
     }
   }
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  // get the form elements defined in your form HTML above
+
+  let form = document.getElementById("contact-form");
+  let formButton = document.getElementById("contact-form-button");
+  let formStatus = document.getElementById("contact-form-status");
+
+  // Success and Error functions for after the form is submitted
+  function success() {
+    form.reset();
+    formButton.classList.remove("button-active");
+    formButton.classList.add("button-inactive");
+    formButton.classList.add("disabled");
+    formStatus.innerHTML =
+      "Thank you for your message! We will get in touch with you as soon as possible.";
+    showMessage();
+  }
+
+  function error() {
+    formStatus.innerHTML =
+      "We are sorry! The message could not be sent, please try again. If the problem persists, you can reach to us by our social networks!";
+    showMessage();
+  }
+
+  // handle the form submission event
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var data = new FormData(form);
+    if (!formButton.classList.contains("disabled")) {
+      ajax(form.method, form.action, data, success, error);
+    }
+  });
+});
+
+// helper function for sending an AJAX request
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+function validateForm() {
+  const inputs = document.querySelectorAll(".field input");
+  const textArea = document.querySelector(".field textarea");
+  const formButton = document.getElementById("contact-form-button");
+  let formElements = Array.prototype.slice.call(inputs);
+  formElements.push(textArea);
+  let validName = false;
+  let validEmail = false;
+  let validText = true;
+
+  // TODO: reset textarea in largeScreenCode (not adding a new listener)
+  window.addEventListener("DOMContentLoaded", () => {
+    textArea.value = "";
+    // formElements.forEach(formEl => {
+    //   formEl.setCustomValidity("");
+    // });
+  });
+
+  formElements.forEach(formEl => {
+    let visitedFlag = false;
+    formEl.addEventListener("keyup", () => {
+      let formElType = formEl.dataset.type;
+      if (formElType === "name") {
+        if (formEl.value.length !== 0) visitedFlag = true;
+        if (visitedFlag) {
+          validName = validateName(formEl);
+          if (validName) {
+            formEl.classList.remove("input-error");
+            formEl.classList.add("input-correct");
+          } else {
+            formEl.classList.remove("input-correct");
+            formEl.classList.add("input-error");
+          }
+        }
+      }
+      if (formElType === "email") {
+        if (formEl.value.length !== 0) visitedFlag = true;
+        if (visitedFlag) {
+          validEmail = validateEmail(formEl);
+          if (validEmail) {
+            formEl.classList.remove("input-error");
+            formEl.classList.remove("input-error-email");
+            formEl.classList.add("input-correct");
+          } else {
+            formEl.classList.add("input-error");
+            formEl.classList.add("input-error-email");
+            formEl.classList.remove("input-correct");
+          }
+        }
+      }
+      if (formElType === "text") {
+        if (formEl.value.length !== 0) visitedFlag = true;
+        if (visitedFlag) {
+          validText = validateText(formEl);
+          if (validText) {
+            formEl.classList.add("input-correct");
+            formEl.classList.remove("input-error");
+          } else {
+            formEl.classList.add("input-error");
+            formEl.classList.remove("input-correct");
+          }
+        }
+      }
+      if (validName && validEmail && validText) {
+        formButton.classList.add("button-shake");
+        formButton.classList.add("button-active");
+        formButton.classList.remove("button-inactive");
+        formButton.classList.remove("disabled");
+      } else {
+        formButton.classList.add("disabled");
+        formButton.classList.add("button-inactive");
+        formButton.classList.remove("button-active");
+        formButton.classList.remove("button-shake");
+      }
+    });
+  });
+}
+
+validateForm();
+
+function validateName(element) {
+  if (
+    element.value.length < 1 ||
+    element.value.length > 50 ||
+    !element.value.replace(/\s+/g, "").length
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function validateEmail(element) {
+  if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(element.value) &&
+    element.value.length < 50
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function validateText(element) {
+  if (element.value.length > 300) {
+    return false;
+  }
+  return true;
+}
+
+function showMessage() {
+  // const message = document.querySelector(".form-fields .field-message");
+  const message = document.querySelector(".field-status");
+  message.classList.toggle("visible");
+
+  setTimeout(() => {
+    message.classList.toggle("visible");
+  }, 8000);
 }
 
 //// /////////// /////
@@ -195,3 +368,18 @@ function editStyle(className, property) {
 // let scrolledY = siteWrapper.scrollTop;
 // console.log(scrolledY);
 // }
+
+// TODO:
+
+// shadows
+// navbar mobile open bug (z-index)
+// bug in navbar when page reloads in desktop? (see nav-white and nav-no-border classes)
+// Logos and svg background (bottom on mobile)
+// check if elses (reduce them)
+// when navbar mobile opens, click everywhere to close it.
+// accesibility svg titles - svg sprite
+// inline svg catched?! see css tricks tutorial
+// grunt?
+// page loader!!
+// dynamic text! See youtube programming video!!
+// social network in navbar?
