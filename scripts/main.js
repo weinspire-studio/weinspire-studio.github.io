70c6244c9295879432a118f5ddf50c693abacd0c
matias <matias.gonzalo.lopez@gmail.com>
2020-03-17 18:05:24 -0400

    last works mobile swiper.js

--- scripts/main.js
+++ scripts/main.js
@@ -384,7 +384,6 @@ var swiper = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination"
   },
-
   navigation: {
     nextEl: "swiper-button-next",
     prevEl: "swiper-button-prev"
