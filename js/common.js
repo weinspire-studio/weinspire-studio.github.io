6f08656d3aaa50f4f0aafba5d66c4f5b619fef4f
matias <matias.gonzalo.lopez@gmail.com>
2020-03-28 00:40:13 -0400

    Iphone navbar bug fix 14

--- js/common.js
+++ js/common.js
@@ -67,14 +67,15 @@ var swiper;
 jQueryModule.smoothScroll();
 contactModule.validateContactForm();
 contactModule.submitContactForm(); //on pageload, executes the following code, depending on screen width.
+// document.addEventListener("DOMContentLoaded", function() {
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (notMobileScreenMQ.matches) {
-    desktopCode();
-  } else {
-    mobileCode();
-  }
-}); //adds listener that executes code when screen width changes (passing by 801px).
+if (notMobileScreenMQ.matches) {
+  desktopCode();
+} else {
+  mobileCode();
+} // });
+//adds listener that executes code when screen width changes (passing by 801px).
+
 
 notMobileScreenMQ.addListener(function () {
   if (notMobileScreenMQ.matches) {
