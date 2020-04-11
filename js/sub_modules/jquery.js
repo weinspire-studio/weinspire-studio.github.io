e36f52d1a33c1d0c8362776e57bff7df4ab95ad4
matias <matias.gonzalo.lopez@gmail.com>
2020-04-11 18:00:14 -0400

    jquery custom + refactor

--- js/sub_modules/jquery.js
+++ js/sub_modules/jquery.js
@@ -1,46 +1,41 @@
 // jshint esversion: 6
 
+// import "../../node_custom_modules/jquery/jquery.min.js";
+import $ from "../../node_custom_modules/jquery/jquery.min.js";
+
 let hasHoverListenerOnPortolio = false;
 
-// jQuery for animated scroll.
+// jQuery for animated scroll
 function smoothScroll() {
-  $("#up-arrow").on("click", function() {
+  $("#up-arrow").on("click", function () {
     const siteWrapperTop = $("#site-wrapper").position().top;
     $("#site-wrapper").animate(
       {
-        scrollTop: siteWrapperTop
+        scrollTop: siteWrapperTop,
       },
       750
     );
   });
 }
 
+// adds hover effect on desktop
 function animateImages() {
   $("#section-projects-design li").hover(
-    function() {
+    function () {
       $(this).addClass("expanded");
-      $(this)
-        .siblings()
-        .addClass("contracted");
-      $(this)
-        .children()
-        .eq(1)
-        .addClass("show-caption");
+      $(this).siblings().addClass("contracted");
+      $(this).children().eq(1).addClass("show-caption");
     },
-    function() {
+    function () {
       $(this).removeClass("expanded");
-      $(this)
-        .siblings()
-        .removeClass("contracted");
-      $(this)
-        .children()
-        .eq(1)
-        .removeClass("show-caption");
+      $(this).siblings().removeClass("contracted");
+      $(this).children().eq(1).removeClass("show-caption");
     }
   );
   hasHoverListenerOnPortolio = true;
 }
 
+// unbinds listeners to desktop component (for mobile cube)
 function unbindImages() {
   if (hasHoverListenerOnPortolio) {
     $("#section-projects-design li").unbind("mouseenter mouseleave");
--- js/sub_modules/swiper.js
+++ js/sub_modules/swiper.js
@@ -1,42 +1,31 @@
 // jshint esversion: 6
 
-// import Swiper from "swiper";
 import "../../node_custom_modules/swiper/css/swiper.min.css";
 import {
   Swiper,
   EffectCube,
-  Pagination
+  Pagination,
 } from "../../node_custom_modules/swiper/js/swiper.esm.js";
 
 Swiper.use([EffectCube, Pagination]);
 
-var defineSwiper = function() {
+var defineSwiper = function () {
   let swiper = new Swiper(".swiper-container", {
     init: false,
-    // pagination: ".swiper-pagination",
-    // slidesPerView: "auto",
-    // paginationClickable: true,
-    // spaceBetween: 0
     effect: "cube",
     keyboardControl: true,
     mousewheelControl: true,
-    // loop: true,
     grabCursor: true,
+    // loop: true,
     cubeEffect: {
       shadow: false,
-      slideShadows: true
-      // shadowOffset: 20,
-      // shadowScale: 0.94
+      slideShadows: true,
     },
     pagination: {
       el: ".swiper-pagination",
-      type: "bullets"
-    }
-    // navigation: {
-    //   nextEl: ".swiper-button-next2",
-    //   prevEl: ".swiper-button-prev2"
-    // }
-    // watchOverflow: false
+      type: "bullets",
+      clickable: true,
+    },
   });
   return swiper;
 };
